720786e06bb95409c71f013346877c3f7c26f23f
shivamj2603 <shivamj2603@gmail.com>
2020-10-29 01:24:31 +0530

    Added:Functionality to check for players with best bowling strike rates

--- src/main/java/com/ipl/StatisticsAnalyser.java
+++ src/main/java/com/ipl/StatisticsAnalyser.java
@@ -75,6 +75,11 @@ public class StatisticsAnalyser {
 		bowlingStatsList.removeIf(stat -> stat.avg == 0);
 		return this.sort(bowlingStatsList, statComparator);
 	}
+	public <E>List getBestBowlingStrikeRate() throws StatisticsAnalyserException {
+		Comparator<BowlingCSV> statComparator = Comparator.comparing(stat -> (stat.strikeRate) );
+		bowlingStatsList.removeIf(stat -> stat.strikeRate == 0);
+		return this.sort(bowlingStatsList, statComparator);
+	}
 	private <E> List sort(List<E> statList, Comparator<E> statComparator) throws StatisticsAnalyserException {
 		if(statList == null || statList.size() == 0) {
 			throw new StatisticsAnalyserException("No Census Data", StatisticsAnalyserException.ExceptionType.NO_STATISTICS_DATA);
