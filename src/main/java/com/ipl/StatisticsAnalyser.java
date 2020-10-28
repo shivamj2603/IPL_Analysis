fdf3b6ff981d9c82f4fd1aba831edc48deb4cc8b
shivamj2603 <shivamj2603@gmail.com>
2020-10-29 01:42:47 +0530

    Added:Functionality to check for players with best strike rate having 4 or 5 wicket hauls

--- src/main/java/com/ipl/StatisticsAnalyser.java
+++ src/main/java/com/ipl/StatisticsAnalyser.java
@@ -84,6 +84,11 @@ public class StatisticsAnalyser {
 		Comparator<BowlingCSV> statComparator = Comparator.comparing(stat -> (stat.economy) );
 		return this.sort(bowlingStatsList, statComparator);
 	}
+	public <E>List getBestBowlingStrikeRateWithHauls() throws StatisticsAnalyserException {
+		Comparator<BowlingCSV> statComparator = Comparator.comparing(stat -> (stat.strikeRate) );
+		bowlingStatsList.removeIf(stat -> (stat.strikeRate == 0 || (stat.fourWkts == 0 && stat.fiveWkts == 0)));
+		return this.sort(bowlingStatsList, statComparator);
+	}
 	private <E> List sort(List<E> statList, Comparator<E> statComparator) throws StatisticsAnalyserException {
 		if(statList == null || statList.size() == 0) {
 			throw new StatisticsAnalyserException("No Census Data", StatisticsAnalyserException.ExceptionType.NO_STATISTICS_DATA);
