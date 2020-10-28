f651cfc2c54d7298f93e4585c1fc7376d20134d5
shivamj2603 <shivamj2603@gmail.com>
2020-10-28 18:40:43 +0530

    Refactored:to delete extra spacing

--- src/main/java/com/ipl/StatisticsAnalyser.java
+++ src/main/java/com/ipl/StatisticsAnalyser.java
@@ -5,9 +5,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.List;
-
 import com.google.gson.Gson;
-
 import CSVBuilder.CSVBuilderException;
 import CSVBuilder.CSVBuilderFactory;
 import CSVBuilder.ICSVBuilder;
--- src/test/java/com/ipltest/StatisticsAnalyserTest.java
+++ src/test/java/com/ipltest/StatisticsAnalyserTest.java
@@ -1,14 +1,11 @@
 package com.ipltest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
-
 import com.google.gson.Gson;
 import com.ipl.BattingCSV;
 import com.ipl.StatisticsAnalyser;
 import com.ipl.StatisticsAnalyserException;
-
 import CSVBuilder.CSVBuilderException;
 
 public class StatisticsAnalyserTest {
